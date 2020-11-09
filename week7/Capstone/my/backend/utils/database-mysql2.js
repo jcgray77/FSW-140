dc236fb5a37593dc1cfc9ebd587128c6b1153f15
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-11-08 20:45:10 -0500

    week7

---
+++ week7/Capstone/my/backend/utils/database-mysql2.js
@@ -0,0 +1,17 @@
+const  mysql = require('mysql2/promise');
+const  {MYSQL_CONFIG}=require('./database-variables.js')
+
+async function mysql2query(sql) {
+  // create the connection
+  const connection = await mysql.createConnection({
+                host     : MYSQL_CONFIG.host,
+              	user     : MYSQL_CONFIG.user,
+              	password : MYSQL_CONFIG.password,
+              	database : MYSQL_CONFIG.database});
+  // query database
+  // console.log(sql);
+  const [rows, fields] = await connection.execute(sql);
+  return [rows, fields];
+}
+
+module.exports = {mysql2query};
--- week7/Capstone/my/src/index.js
+++ week7/Capstone/my/src/index.js
@@ -1,42 +1,47 @@
-import './styles/index.scss';
-
-import * as moment from 'moment';
-// Add more locales
-import '../node_modules/moment/locale/en-gb';
-moment.locale(['en-GB']);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
-import App from './containers/App';
-import { history } from './services';
+// import './index.css';
+import {BrowserRouter, Route, Switch, withRouter, Redirect} from 'react-router-dom';
 
-// Post logout redirect logic*-+9/
-if (window.location.href.indexOf('?postLogout=true') > 0) {
-  const { AuthService } = require('./services/auth.service');
-  var authService = new AuthService();
-  authService.signoutRedirectCallback().then(() => {
-    // clear the query string
-    history.push('/', '');
-  });
-}
+import {NotificationContainer} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+import SideBar from './components/SideBar.js';
+import NaviBar from './components/NaviBar.js';
 
-function render() {
-  ReactDOM[window.ssrEnabled ? 'hydrate' : 'render'](
-    <Router history={history}>
-      <App />
-    </Router>,
-    document.getElementById('app')
-  );
-}
+import MainCard from './components/main/MainCard.js'
+import ProfileCard from './components/student/ProfileCard.js'
+import {AutoTimeTable, CartTimeTable} from './components/timetable/TimeTable.js'
+import AppointmentCard from './components/appointments/AppointmentCard.js'
+import Callback from './components/Callback.js'
+import registerServiceWorker from './registerServiceWorker.js';
 
-render();
+import Auth from './Auth/Auth';
 
-if (module.hot) {
-  module.hot.accept('./containers/App', () => {
-    const NextApp = require('./containers/App');
-    console.clear();
-    render(NextApp);
-  });
-}
+const auth=new Auth();
+
+ReactDOM.render(
+	<div>
+	<BrowserRouter>
+		<div>
+			<NotificationContainer/>
+			<Route  path='/' render={withRouter((props)=><SideBar auth={auth} {...props}/>)}/>
+			<div class="main-panel">
+				<NaviBar auth={auth}/>
+				<div class="content">
+					<Switch>
+						<Route path='/profile'  render={(props)=>auth.isAuthenticated()?<ProfileCard auth={auth} {...props}/>:<Redirect to='/'/>}/>
+						<Route path='/auto' render={(props)=><AutoTimeTable auth={auth} {...props}/>}/>
+						<Route path='/cart' render={(props)=><CartTimeTable auth={auth} {...props}/>}/>
+						<Route path='/appointments'  render={(props)=>auth.isAuthenticated()?<AppointmentCard auth={auth} {...props}/>:<Redirect to='/'/>}/>
+						<Route path='/callback' render={(props)=><Callback auth={auth} {...props}/>}/>
+						<Route path='/' render={(props)=><MainCard auth={auth} {...props}/>}/>
+					</Switch>
+				</div>
+			</div>
+		</div>
+	</BrowserRouter>
+	</div>,
+	document.getElementById('root')
+	);
+
+registerServiceWorker();
